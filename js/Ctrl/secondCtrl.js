e6544373bd865b1c9eb7c45867e8fc157a56e818
Brian Stocking <brianm.stocking@gmail.com>
2015-03-06 10:06:02 -0700

    got the five different range inputs posting thier own dta

--- js/Ctrl/secondCtrl.js
+++ js/Ctrl/secondCtrl.js
@@ -6,7 +6,6 @@ var app = angular.module('poopApp')
 app.controller('CtrlThree', function($scope, $firebase, secondService, $filter){
 
 
-
     var ref = new Firebase("https://ipoop.firebaseio.com/"+ secondService.date +"/con");
 
     $scope.conFun = function(){
